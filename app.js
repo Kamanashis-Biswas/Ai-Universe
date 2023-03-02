021ac194e2038eedde7e72e0f0fe0b3f78133580
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-03-03 03:16:45 +0600

    Dynamic Product Name and Date Show and add Details Button

--- app.js
+++ app.js
@@ -8,22 +8,30 @@ const loadProducts = async() => {
 
 const displayProducts=products=>{
     const productContainer=document.getElementById('product-container');
-    products.forEach(product=>{
+    productContainer.innerHTML='';
+    products.slice(0,6).forEach(product=>{
         const productDiv=document.createElement('div');
         productDiv.classList.add('col');
         productDiv.innerHTML=`
-        <div class="card">
+        <div class="card p-2">
             <img src="${product.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${product.name}</h5>
+                <hr>
+                <div class="d-flex justify-content-between align-items-cente">
+                <p class="card-text"><i class="fa-solid fa-calendar-days"></i>${product.published_in}</p>
+                <button class="btn btn-light rounded-circle"><i class="fa-solid fa-arrow-right"></i></button>
+                </div>
             </div>
         </div>
         `;
         productContainer.appendChild(productDiv);
     })
 }
+
+document.getElementById('sort-btn').addEventListener('click',function(){
+    console.log("Sort Btn Click");
+})
 
 
 
