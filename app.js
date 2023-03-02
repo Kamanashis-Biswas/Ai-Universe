6995a5859f7962649966f631e2ae4889b43d8a78
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-03-03 03:51:12 +0600

    Spinner Add

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const loadProducts = async() => {
 const displayProducts=products=>{
     const productContainer=document.getElementById('product-container');
     productContainer.innerHTML='';
-    products.slice(0,6).forEach(product=>{
+    //display 6 product
+    products=products.slice(0,6);
+    products.forEach(product=>{
+        toggleSpinner(true);
         const productDiv=document.createElement('div');
         productDiv.classList.add('col');
         productDiv.innerHTML=`
@@ -18,7 +21,7 @@ const displayProducts=products=>{
             <div class="card-body">
                 <h5 class="card-title">${product.name}</h5>
                 <hr>
-                <div class="d-flex justify-content-between align-items-cente">
+                <div class="d-flex justify-content-between align-items-center">
                 <p class="card-text"><i class="fa-solid fa-calendar-days"></i>${product.published_in}</p>
                 <button class="btn btn-light rounded-circle"><i class="fa-solid fa-arrow-right"></i></button>
                 </div>
@@ -26,42 +29,22 @@ const displayProducts=products=>{
         </div>
         `;
         productContainer.appendChild(productDiv);
-    })
+    });
+    // stop loader
+    toggleSpinner(false);
 }
 
 document.getElementById('sort-btn').addEventListener('click',function(){
     console.log("Sort Btn Click");
 })
 
-
-
 loadProducts();
 
-// const showData = (data) => {
-//     // console.log(data.data.tools)
-//     // for(let singleData of data.data.tools.slice(0,6)){
-//     //     console.log(singleData);
-//     // }
-//     const productContainer=document.getElementById('product-info');
-//     data.data.tools.slice(0, 6).forEach((product) => {
-//         console.log(product.image)
-//        const div=document.createElement('div')
-//        div.innerHTML=`
-//        <div class="row row-cols-1 row-cols-md-2 g-4">
-//        <div class="col">
-//            <div class="card">
-//                <img src="${product.image}" class="card-img-top" alt="...">
-//                <div class="card-body">
-//                    <h5 class="card-title">Card title</h5>
-//                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-//                        to additional content. This content is a little bit longer.</p>
-//                </div>
-//            </div>
-//        </div>
-//    </div>
-//        `;
-//        productContainer.appendChild(div);
-//     });
-
-// };
-
+const toggleSpinner=isLoading=>{
+    const loaderSection=document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }else{
+        loaderSection.classList.add('d-none');
+    }
+}
