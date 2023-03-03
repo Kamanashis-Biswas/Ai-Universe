dbcb1cbd0b5882abdab05bae1d12335eaebc946d
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-03-03 13:13:28 +0600

    Modal Add

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const loadProducts = async() => {
-    const url=`https://openapi.programming-hero.com/api/ai/tools`
-    const res=await fetch(url);
-    const data=await res.json();
+const loadProducts = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
     displayProducts(data.data.tools)
-    
+
 }
 
-const displayProducts=products=>{
-    const productContainer=document.getElementById('product-container');
-    productContainer.innerHTML='';
+const displayProducts = products => {
+    const productContainer = document.getElementById('product-container');
+    productContainer.innerHTML = '';
     //display 6 product
-    products=products.slice(0,6);
-    products.forEach(product=>{
+    products = products.slice(0, 6);
+    products.forEach(product => {
         toggleSpinner(true);
-        const productDiv=document.createElement('div');
+        const productDiv = document.createElement('div');
         productDiv.classList.add('col');
-        productDiv.innerHTML=`
+        productDiv.innerHTML = `
         <div class="card p-2">
             <img src="${product.image}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -23,7 +23,7 @@ const displayProducts=products=>{
                 <hr>
                 <div class="d-flex justify-content-between align-items-center">
                 <p class="card-text"><i class="fa-solid fa-calendar-days"></i>${product.published_in}</p>
-                <button class="btn btn-light rounded-circle"><i class="fa-solid fa-arrow-right"></i></button>
+                <button onclick="loadProductDetails('${product.id}')" class="btn btn-light rounded-circle" data-bs-toggle="modal" data-bs-target="#productDetailModal"><i class="fa-solid fa-arrow-right"></i></button>
                 </div>
             </div>
         </div>
@@ -34,17 +34,24 @@ const displayProducts=products=>{
     toggleSpinner(false);
 }
 
-document.getElementById('sort-btn').addEventListener('click',function(){
+document.getElementById('sort-btn').addEventListener('click', function () {
     console.log("Sort Btn Click");
 })
 
 loadProducts();
 
-const toggleSpinner=isLoading=>{
-    const loaderSection=document.getElementById('loader');
-    if(isLoading){
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
         loaderSection.classList.remove('d-none');
-    }else{
+    } else {
         loaderSection.classList.add('d-none');
     }
+}
+
+const loadProductDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
 }
