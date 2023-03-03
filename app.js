36ee0eb9fd068fc2e100eecbb1ac627b6a0c1719
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-03-03 15:33:15 +0600

    card feature add

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//data load
 const loadProducts = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
@@ -19,11 +20,17 @@ const displayProducts = products => {
         <div class="card p-2">
             <img src="${product.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <hr>
+            <h5 class="card-title fw-bold">Features</h5>
+            <ol>
+                <li>${product.features}</li>
+                <li></li>
+                <li></li>
+            </ol>
+            <hr>
+            <h5 class="card-title">${product.name}</h5>
                 <div class="d-flex justify-content-between align-items-center">
-                <p class="card-text"><i class="fa-solid fa-calendar-days"></i>${product.published_in}</p>
-                <button onclick="loadProductDetails('${product.id}')" class="btn btn-light rounded-circle" data-bs-toggle="modal" data-bs-target="#productDetailModal"><i class="fa-solid fa-arrow-right"></i></button>
+                <p class="card-text"><i class="fa-solid fa-calendar-days me-2"></i>${product.published_in}</p>
+                <button onclick="loadProductDetails(${product.id})" class="btn btn-light rounded-circle" data-bs-toggle="modal" data-bs-target="#productDetailModal"><i class="fa-solid fa-arrow-right"></i></button>
                 </div>
             </div>
         </div>
@@ -53,5 +60,5 @@ const loadProductDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    console.log(data);
 }
